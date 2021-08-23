b909e0351555cf267f014eacf19b9f60f0f6c13e
jebranali <ali.blaster7@gmail.com>
2021-08-24 00:33:10 +0500

    added send invitation method.

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\InvitationRequest;
+use App\Models\Invitation;
 use Illuminate\Http\Request;
 
 class InvitationController extends Controller
 {
-    //
+    public function sendInvitation(InvitationRequest $request)
+    {
+        $invitation = new Invitation($request->all());
+        $invitation->generateInvitationToken();
+        $invitation->save();
+    }
 }
