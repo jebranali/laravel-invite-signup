56361bb66ca671e576bdb6062682bd87c9f366c8
jebranali <ali.blaster7@gmail.com>
2021-08-24 00:32:35 +0500

    generate token.

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -12,4 +12,9 @@ class Invitation extends Model
     protected $fillable = [
         'email', 'invitation_token', 'registered_at',
     ];
+
+    public function generateInvitationToken()
+    {
+        $this->invitation_token = substr(md5(rand(0, 9) . $this->email . time()), 0, 32);
+    }
 }
