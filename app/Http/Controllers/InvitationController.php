9d931ff65d8a4fc7a4129fb3377d213e8c354b3a
jebranali <ali.blaster7@gmail.com>
2021-08-24 01:03:55 +0500

    sent invitation.

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\InvitationRequest;
+use App\Mail\SendInvite;
 use App\Models\Invitation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class InvitationController extends Controller
 {
+    /**
+     * @param InvitationRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function sendInvitation(InvitationRequest $request)
     {
         $invitation = new Invitation($request->all());
         $invitation->generateInvitationToken();
         $invitation->save();
+        $data = $invitation->toArray();
+
+        Mail::to($data['email'])->send(new SendInvite($data));
+
+        return response()->json("Invitation sent successfully", 200);
     }
 }
