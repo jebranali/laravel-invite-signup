451b1c33b19fdfbb0410f35170294f820ae1b76c
jebranali <ali.blaster7@gmail.com>
2021-08-24 23:52:11 +0500

    profile update request.

---
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ProfileUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:6',
+            'user_name' => 'required|required|min:4|max:20',
+            'avatar' => 'mimes:jpeg|required|dimensions:min_width=256,min_height=256'
+        ];
+    }
+
+    /**
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+}
