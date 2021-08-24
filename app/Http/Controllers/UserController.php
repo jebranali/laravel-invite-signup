d5b062b558bb374752d797109f25614eead41c7c
jebranali <ali.blaster7@gmail.com>
2021-08-24 22:46:33 +0500

    added login method.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
-
+use Illuminate\Validation\ValidationException;
 class UserController extends Controller
 {
     /**
@@ -39,5 +39,23 @@ class UserController extends Controller
 
             return response()->json("Kindly verify your pin code", 200);
         }
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $user->createToken('token')->plainTextToken;
     }
 }
